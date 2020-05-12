4daa16a64d25275d5b03359172fa655d322670c6
hotnotHD <de-kovalevs@yandex.ru>
2020-05-12 15:50:19 +0300

    V2.5 con i test

--- src/main/java/FlagCont.java
+++ src/main/java/FlagCont.java
@@ -1,14 +1,74 @@
 import java.util.Objects;
 
 public class FlagCont {
-    public boolean register;
-    public boolean uniq;
-    public boolean repeatNum;
-    public boolean ignoreIntb = false;
-    public int ignoreInt = 0;
-    public boolean outputFile = false;
-    public String outputFileS = "";
-    public String inputFileS = "";
+    private boolean register;
+    private boolean uniq;
+    private boolean repeatNum;
+    private boolean ignoreIntb;
+    private int ignoreInt;
+    private boolean outputFile;
+    private String outputFileS;
+    private String inputFileS;
+
+    public FlagCont (String[] inputF) {
+        for (String i: inputF){
+            if (ignoreIntb) {
+                try {
+                    this.ignoreInt = Integer.parseInt(i);
+                    this.ignoreIntb = false;
+                } catch (NumberFormatException e) {
+                    System.err.println(e);
+                }
+            }
+            switch (i) {
+                case "-i":
+                    this.register = true;
+                    break;
+                case "-u":
+                    this.uniq = true;
+                    break;
+                case "-c":
+                    this.repeatNum = true;
+                    break;
+                case "-s":
+                    this.ignoreIntb = true;
+                    break;
+                case "-o":
+                    this.outputFile = true;
+                    break;
+            }
+            if (outputFile) {
+                this.outputFileS = i;
+                this.outputFile = false;
+            }
+            if (i.charAt(0) != '-') {
+                this.inputFileS = i;
+            }
+        }
+    }
+    public boolean getRegister() {
+        return register;
+    }
+
+    public boolean getUniq() {
+        return uniq;
+    }
+
+    public boolean getRepeatNum() {
+        return repeatNum;
+    }
+
+    public int getIgnoreInt() {
+        return ignoreInt;
+    }
+
+    public String getInputFileS() {
+        return inputFileS;
+    }
+
+    public String getOutputFileS() {
+        return outputFileS;
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/UniqApp.java
+++ src/main/java/UniqApp.java
@@ -3,66 +3,20 @@ import java.io.*;
 public class UniqApp {
 
     public static void starter(String[] input) throws IOException {
-        FlagCont flags = new FlagCont();
+        FlagCont flags = new FlagCont(input);
         try {
-            for (String i : input) {
-                flags(i, flags);
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid input of arguments");
-        }
-        try {
-            File file = !flags.outputFileS.isEmpty() ? new File(flags.outputFileS) : null;
+            File file = flags.getOutputFileS() != null ? new File(flags.getOutputFileS()) : null;
             FileWriter fW = file != null ? new FileWriter(file) : null;
             BufferedWriter bW = fW != null ? new BufferedWriter(fW) : new BufferedWriter(new OutputStreamWriter(System.out));
-            File file2 = !flags.inputFileS.isEmpty() ? new File(flags.inputFileS) : null;
+            File file2 = flags.getInputFileS() != null ? new File(flags.getInputFileS()) : null;
             FileReader fR = file2 != null ? new FileReader(file2) : null;
             BufferedReader bR = fR != null ? new BufferedReader(fR) : new BufferedReader(new InputStreamReader(System.in));
             uniqBase(bW, bR, flags);
             bW.close();
             bR.close();
         } catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
+            System.err.println(e);
         }
-    }
-
-    public static void flags(String inputF, FlagCont flags) {
-        switch (inputF) {
-            case "-i":
-                flags.register = true;
-                return;
-            case "-u":
-                flags.uniq = true;
-                return;
-            case "-c":
-                flags.repeatNum = true;
-                return;
-            case "-s":
-                flags.ignoreIntb = true;
-                return;
-            case "-o":
-                flags.outputFile = true;
-                return;
-        }
-        if (flags.ignoreIntb) {
-            try {
-                flags.ignoreInt = Integer.parseInt(inputF);
-            } catch (NumberFormatException e) {
-                System.out.println("Wrong input");
-            }
-            flags.ignoreIntb = false;
-            return;
-        }
-        if (flags.outputFile) {
-            flags.outputFileS = inputF;
-            flags.outputFile = false;
-            return;
-        }
-        if (inputF.charAt(0) != '-') {
-            flags.inputFileS = inputF;
-            return;
-        }
-        throw new IllegalArgumentException();
     }
 
     private static void uniqBase(BufferedWriter bW, BufferedReader bR, FlagCont flags) throws IOException {
@@ -81,20 +35,20 @@ public class UniqApp {
                 lastString = false;
             } else line1 = reader(bR);
             if (!skip && line1 != null) {
-                if (flags.register) {
+                if (flags.getRegister()) {
                     line1 = line1.toLowerCase();
                     line2 = line2.toLowerCase();
                 }
                 int b = line1.length();
                 int a = line2.length();
-                int ignore = flags.ignoreInt;
+                int ignore = flags.getIgnoreInt();
                 if (ignore > b || ignore > a) {
                     ignore = Math.min(a, b);
                 }
                 if (line1.substring(ignore).equals(line2.substring(ignore))) count += 1;
                 else {
-                    if (!flags.uniq || count == 1) {
-                        if (flags.repeatNum && !line2.isEmpty()) writer(bW, count + " " + line2);
+                    if (!flags.getUniq() || count == 1) {
+                        if (flags.getRepeatNum() && !line2.isEmpty()) writer(bW, count + " " + line2);
                         else writer(bW, line2);
                     }
                     count = 1;
